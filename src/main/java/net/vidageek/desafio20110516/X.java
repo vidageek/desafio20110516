f1e091cad129c9520dc056bb9bc256caf198ee04
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2011-05-16 22:43:57 -0300

    moving expression from for loop to loop body, to enable further refactorings

--- src/main/java/net/vidageek/desafio20110516/X.java
+++ src/main/java/net/vidageek/desafio20110516/X.java
@@ -1,23 +1,23 @@
 package net.vidageek.desafio20110516;
 
 final public class X {
+    public static long f(int n) {
+        String a = "01", b = "0";
+        while (n-- > 0) {
+            String c = b;
+            b = a;
+            a = b + c;
+        }
+        assert n == -1;
+        return stringToLong(b);
+    }
 
-	public static long f(int n) {
-		String a = "01", b = "0";
-		while (n-- > 0) {
-			String c = b;
-			b = a;
-			a = b + c;
-		}
-		assert n == -1;
-		return stringToLong(b);
-	}
 
     private static long stringToLong(String b) {
         long l = 0;
-		for (int i = 0; i < b.length(); l += (b.charAt(b.length() - i + -1) - 48) * (1L << i++)) {
-		}
-		return l;
+        for (int i = 0; i < b.length();) {
+            l += (b.charAt(b.length() - i + -1) - 48) * (1L << i++);
+        }
+        return l;
     }
-
 }

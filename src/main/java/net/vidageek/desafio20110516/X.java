cc4c2e93f6363b8db71de63baddce566e384a313
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2011-05-16 22:59:11 -0300

    unroll loop to enable further refactorings

--- src/main/java/net/vidageek/desafio20110516/X.java
+++ src/main/java/net/vidageek/desafio20110516/X.java
@@ -3,6 +3,11 @@ package net.vidageek.desafio20110516;
 final public class X {
     public static long f(int n) {
         String a = "01", b = "0";
+        if (n-- > 0) {
+            String c = b;
+            b = a;
+            a = b + c;
+        }
         while (n-- > 0) {
             String c = b;
             b = a;

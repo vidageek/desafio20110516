c67d99b600913b4a3406db626926684cf5c9693f
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2011-05-16 23:23:42 -0300

    extract method to have an auxiliar fibonacci function

--- src/main/java/net/vidageek/desafio20110516/X.java
+++ src/main/java/net/vidageek/desafio20110516/X.java
@@ -1,9 +1,19 @@
 package net.vidageek.desafio20110516;
 
 final public class X {
+    private static long fib(int n) {
+        int x = 0, y = 1;
+        while (n-- > 0) {
+            int z = y;
+            y = x + y;
+            x = z;
+        }
+        return y;
+    }
+
+
     public static long f(int n) {
         String a = "01", b = "0";
-        int x = 0, y = 1;
         if (n-- > 0) {
             String c = "0";
             assert 0 == stringToLong(c);
@@ -11,10 +21,7 @@ final public class X {
             assert 1 == stringToLong(b);
             a = "01" + "0";
             assert 1 * 2 + 0 == stringToLong(a);
-            int z = y;
-            y = x + y;
-            x = z;
-            assert Math.pow(2, y) == 2;
+            assert Math.pow(2, fib(1)) == 2;
         }
         if (n-- > 0) {
             String c = "01";
@@ -23,10 +30,7 @@ final public class X {
             assert 2 == stringToLong(b);
             a = "010" + "01";
             assert 2 * 4 + 1 == stringToLong(a);
-            int z = y;
-            y = x + y;
-            x = z;
-            assert Math.pow(2, y) == 4;
+            assert Math.pow(2, fib(2)) == 4;
         }
         if (n-- > 0) {
             String c = "010";
@@ -35,10 +39,7 @@ final public class X {
             assert 8 + 1 == stringToLong(b);
             a = "01001" + "010";
             assert (8 + 1) * 8 + 2 == stringToLong(a);
-            int z = y;
-            y = x + y;
-            x = z;
-            assert Math.pow(2, y) == 8;
+            assert Math.pow(2, fib(3)) == 8;
         }
         if (n-- > 0) {
             String c = "01001";
@@ -47,10 +48,7 @@ final public class X {
             assert 64 + 8 + 2 == stringToLong(b);
             a = "01001010" + "01001";
             assert (64 + 8 + 2) * 32 + (8 + 1) == stringToLong(a);
-            int z = y;
-            y = x + y;
-            x = z;
-            assert Math.pow(2, y) == 32;
+            assert Math.pow(2, fib(4)) == 32;
         }
         if (n-- > 0) {
             String c = "01001010";
@@ -59,10 +57,7 @@ final public class X {
             assert 2048 + 256 + 64 + 8 + 1 == stringToLong(b);
             a = "0100101001001" + "01001010";
             assert (2048 + 256 + 64 + 8 + 1) * 256 + (64 + 8 + 2) == stringToLong(a);
-            int z = y;
-            y = x + y;
-            x = z;
-            assert Math.pow(2, y) == 256;
+            assert Math.pow(2, fib(5)) == 256;
         }
         while (n-- > 0) {
             String c = b;

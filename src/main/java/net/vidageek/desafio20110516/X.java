ee138ce1326015f2995d967f2dc59aca966b0d34
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2011-05-16 23:28:51 -0300

    remove loop unrolling

--- src/main/java/net/vidageek/desafio20110516/X.java
+++ src/main/java/net/vidageek/desafio20110516/X.java
@@ -24,51 +24,6 @@ final public class X {
             z = b;
         }
         String a = "01", b = "0";
-        if (n-- > 0) {
-            String c = "0";
-            assert 0 == stringToLong(c);
-            b = "01";
-            assert 1 == stringToLong(b);
-            a = "01" + "0";
-            assert 1 * 2 + 0 == stringToLong(a);
-            assert Math.pow(2, fib(1)) == 2;
-        }
-        if (n-- > 0) {
-            String c = "01";
-            assert 1 == stringToLong(c);
-            b = "010";
-            assert 2 == stringToLong(b);
-            a = "010" + "01";
-            assert 2 * 4 + 1 == stringToLong(a);
-            assert Math.pow(2, fib(2)) == 4;
-        }
-        if (n-- > 0) {
-            String c = "010";
-            assert 2 == stringToLong(c);
-            b = "01001";
-            assert 8 + 1 == stringToLong(b);
-            a = "01001" + "010";
-            assert (8 + 1) * 8 + 2 == stringToLong(a);
-            assert Math.pow(2, fib(3)) == 8;
-        }
-        if (n-- > 0) {
-            String c = "01001";
-            assert 8 + 1 == stringToLong(c);
-            b = "01001010";
-            assert 64 + 8 + 2 == stringToLong(b);
-            a = "01001010" + "01001";
-            assert (64 + 8 + 2) * 32 + (8 + 1) == stringToLong(a);
-            assert Math.pow(2, fib(4)) == 32;
-        }
-        if (n-- > 0) {
-            String c = "01001010";
-            assert 64 + 8 + 2 == stringToLong(c);
-            b = "0100101001001";
-            assert 2048 + 256 + 64 + 8 + 1 == stringToLong(b);
-            a = "0100101001001" + "01001010";
-            assert (2048 + 256 + 64 + 8 + 1) * 256 + (64 + 8 + 2) == stringToLong(a);
-            assert Math.pow(2, fib(5)) == 256;
-        }
         while (n-- > 0) {
             String c = b;
             b = a;

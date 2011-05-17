4a79a3458b98862d3ae3c8ca6e5da22673febceb
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2011-05-16 23:40:51 -0300

    introduce a println to discover we are generating the wrong parts of the fibonacci sequence

--- src/main/java/net/vidageek/desafio20110516/X.java
+++ src/main/java/net/vidageek/desafio20110516/X.java
@@ -2,12 +2,14 @@ package net.vidageek.desafio20110516;
 
 final public class X {
     private static long fib(int n) {
+        System.out.print("fib(" + n + ") => ");
         int a = 0, b = 1;
-        while (n --> 0) {
+        while (--n > 0) {
             int c = b;
             b = a + b;
             a = c;
         }
+        System.out.println(b);
         return b;
     }
 
@@ -15,9 +17,9 @@ final public class X {
     public static long f(int n) {
         long a = 0, b = 1;
         for (int i = 0; i < n; i++) {
-            long c = a;
-            a = b;
-            b = a * (long) Math.pow(2, fib(i + 1)) + c;
+            long c = b;
+            b = a + b * (long) Math.pow(2, fib(i + 2));
+            a = c;
         }
         return a;
     }
